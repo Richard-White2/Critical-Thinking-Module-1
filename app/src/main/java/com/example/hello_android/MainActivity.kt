78fa87a901c1a881d2e7a2617e417b697ca19cef
Richard White <riwhite50@gmail.com>
2025-04-05 19:00:49 -0400

    Final Commit

---
+++ app/src/main/java/com/example/hello_android/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.hello_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.hello_android.ui.theme.Hello_AndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Hello_AndroidTheme {
+                HelloText()
+                }
+            }
+        }
+    }
+
+@Preview(showBackground = true)
+@Composable
+fun HelloTextPreview() {
+    Hello_AndroidTheme {
+        Column(
+            modifier = Modifier
+                .background(Color.Red)
+                .fillMaxSize()
+                .height(150.dp)
+                .padding(15.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        )
+        {
+            Text(
+                text = "Hello, Android!",
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.bodyMedium,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Black
+            )
+        }
+    }
+}
+
+@Composable
+fun HelloText(){
+    Column(
+        modifier = Modifier
+            .background(Color.Red)
+            .fillMaxSize()
+            .height(150.dp)
+            .padding(15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
+    {
+        Text(
+            text = "Hello, Android!",
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.bodyMedium,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Black
+        )
+    }
+}
